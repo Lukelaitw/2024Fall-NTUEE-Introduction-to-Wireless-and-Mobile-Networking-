5f62c82d5eb33a3543ce5990bfa093b7766f13ec
Lukelaitw <104292677+Lukelaitw@users.noreply.github.com>
2024-11-11 17:11:19 +0800

    Add files via upload

--- b12901075_hw3/check.py
+++ b12901075_hw3/check.py
@@ -1,6 +1,6 @@
 import pandas as pd
 # Replace 'handoff_events_bonus.csv' with the path to your CSV file if it's in a different directory
-csv_filename = 'handoff_events_bonus.csv'
+csv_filename = 'handoff_events.csv'
 df = pd.read_csv(csv_filename)
 row_count = df['Device ID'].count()
 print(f"Total number of rows: {row_count}")
